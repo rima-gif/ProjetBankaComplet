5836c53a597872d60bb5c7e328cfb073c50457f8
rima-gif <achourryma971@gmail.com>
2025-05-20 23:48:41 +0200

    Update BankAccountRestAPI.java

--- back/src/main/java/com/example/ebankingbackend/web/BankAccountRestAPI.java
+++ back/src/main/java/com/example/ebankingbackend/web/BankAccountRestAPI.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://192.168.40.121")
+@CrossOrigin(origins = "*")
 public class BankAccountRestAPI {
     private BankAccountService bankAccountService;
 
