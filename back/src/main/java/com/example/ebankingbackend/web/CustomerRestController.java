519c1e1c03380a5e9314a4fff3a384604031f853
rima-gif <achourryma971@gmail.com>
2025-05-21 00:02:36 +0200

    Update CustomerRestController.java

--- back/src/main/java/com/example/ebankingbackend/web/CustomerRestController.java
+++ back/src/main/java/com/example/ebankingbackend/web/CustomerRestController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://192.168.40.124:30080")
 public class CustomerRestController {
     private BankAccountService bankAccountService;
 
