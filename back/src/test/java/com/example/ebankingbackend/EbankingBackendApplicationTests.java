07693f6577b10f8f86c879b991da9019912f6b2d
rima-gif <achourryma971@gmail.com>
2025-05-21 11:30:46 +0200

    Update EbankingBackendApplicationTests.java

--- back/src/test/java/com/example/ebankingbackend/EbankingBackendApplicationTests.java
+++ back/src/test/java/com/example/ebankingbackend/EbankingBackendApplicationTests.java
@@ -7,9 +7,24 @@ import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest
 
 class EbankingBackendApplicationTests {
+      @Autowired
+    private ApplicationContext applicationContext;
+
 
     @Test
     void contextLoads() {
+         assertNotNull(applicationContext, "Application context should not be null");
+        
+        // Verify key beans are available in the context
+        assertTrue(applicationContext.containsBean("bankAccountService"), 
+            "BankAccountService bean should be available");
+        
+        assertTrue(applicationContext.containsBean("customerRepository"), 
+            "CustomerRepository bean should be available");
+        
+        assertTrue(applicationContext.containsBean("bankAccountRepository"), 
+            "BankAccountRepository bean should be available");
+    }
     }
 
-}
+
